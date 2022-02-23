85ab694235559fd3ec3332aba284e352631daad6
F <kaminoko101@gmail.com>
2022-02-23 22:56:15 +0630

    A working layout

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -5,21 +5,24 @@ import { useEffect, useState } from "react";
 import NewList from "../../components/NewList";
 import List from "../../components/List";
 import Button from "../../components/Button";
+import SideMenu from "../../components/SideMenu";
 
 import { signOut, getSession } from "next-auth/react";
 
 import randomColorGenerator from "../../utils";
 import Loading from "../../components/Loading";
+import DashboardHeader from "../../components/DashboardHeader";
 
 let count = 4;
 
 export default function Dashboard({ _session }) {
+  const { user } = _session;
   const [dummyData, setDummyData] = useState([
     {
       id: 1,
       title: "To-Do",
       theme: "rgb(147 197 253)",
-      items: ["build something", "do something"],
+      items: [],
     },
     { id: 2, title: "Doing", theme: "rgb(249 168 212)", items: [] },
     { id: 3, title: "Done", theme: "rgb(134 239 172)", items: [] },
@@ -57,10 +60,6 @@ export default function Dashboard({ _session }) {
   };
 
   useEffect(() => {
-    console.log(dummyData);
-  }, [dummyData]);
-
-  useEffect(() => {
     if (!_session) {
       router.push("/login");
     }
@@ -69,24 +68,28 @@ export default function Dashboard({ _session }) {
   if (!_session) return <Loading />;
 
   return (
-    <div className="h-screen overflow-x-scroll">
+    <div className="flex h-screen overflow-y-hidden">
       <Head>
         <title>WorkNow</title>
       </Head>
-      <div className="inline-flex">
-        {dummyData.map(({ id, title, theme, items }) => (
-          <List
-            id={id}
-            key={id}
-            title={title}
-            listItems={items}
-            theme={theme}
-            addItem={addItem}
-            updateListTitle={updateListTitle}
-          />
-        ))}
-        <NewList addNewList={addNewList} />
-        <Button title="Logout" type="primary" onPress={() => signOut()} />
+      <SideMenu />
+      <div className="flex flex-col w-4/5">
+        <DashboardHeader user={user} />
+        <div className="inline-flex mt-[51px]">
+          {dummyData.map(({ id, title, theme, items }) => (
+            <List
+              id={id}
+              key={id}
+              title={title}
+              listItems={items}
+              theme={theme}
+              addItem={addItem}
+              updateListTitle={updateListTitle}
+            />
+          ))}
+          <NewList addNewList={addNewList} />
+          <Button title="Logout" type="primary" onPress={() => signOut()} />
+        </div>
       </div>
     </div>
   );
